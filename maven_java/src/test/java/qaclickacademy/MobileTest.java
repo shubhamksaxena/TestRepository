b3a33b949e4dbb418196513789544649b126cfc9
shubhams <shubhams@ShubhamS.positiveedge.com>
2018-05-30 17:29:53 +0530

    New Maven_java project created

---
+++ maven_java/src/test/java/qaclickacademy/MobileTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+  @Test
+  public void mobtest() {
+	  System.out.println("mobtest");
+  }
+@Test
+public void mob2test()
+{
+	System.out.println("mob2 test");
+}
+
+}
---
+++ maven_java/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+  @Test
+  public void seleniumfirsttest() {
+	  System.out.println("seleniumfirsttest");
+  }
+
+@Test
+public void seleniumsecondtest()
+{
+	System.out.println("seleniumsecodntest");
+}
+}
